4c312ea6181ee58612fe0e452c186f736d422a46
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-06 19:06:22 -0400

    top of demo looks ok

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ function App() {
     <>
     <Navbar/>
     <div className="Header--first">
-    <h1 className="Header__Big"> Your product predictions right at your fingertips. </h1>
-    <h2 className="Header__Small"> Let us help you cater to your consumers demands by using our advanced recommendation system based on over <strong>50 million</strong> products.</h2>
+    <h1 className="Header__Big"> Your product playbook right at your fingertips. </h1>
+    <h2 className="Header__Small"> Let us help you cater to your consumer's demands by using our advanced recommendation system based on over <strong>50 million</strong> products.</h2>
     <Button variant="contained" color="secondary" className="Header__Button">DEMO</Button>
+    </div>
+    <div className="Center_Image">
+    <img className="Image--first" src="https://searchengineland.com/figz/wp-content/seloads/2015/10/graph-line-trend-analytics-magnifying-glass-ss-1920.jpg"></img>
     </div>
     </>
   );
