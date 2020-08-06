877488b95d3387693791b2d3bc4e3d058c49b46c
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-06 18:24:45 -0400

    styled the header and app bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
 
 function App() {
   return (
+    <>
     <Navbar/>
+    <div className="Header--first">
+    <h1 className="Header__Big"> Your product predictions right at your fingertips. </h1>
+    <h2 className="Header__Small"> Let us help you cater to your consumers demands by using our advanced recommendation system based on over <strong>50 million</strong> products.</h2>
+    <Button variant="contained" color="secondary" className="Header__Button">DEMO</Button>
+    </div>
+    </>
   );
 }
 
