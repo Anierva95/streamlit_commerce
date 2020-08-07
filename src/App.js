0697d7ea3216092ced8047e93a9beb9eb0205f9e
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-06 22:17:06 -0400

    added grid system for row

--- src/App.js
+++ src/App.js
@@ -3,30 +3,45 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <div class="wrapper">
-    <div className="Header--first">
-    <h1 className="Header__Big"> Your product playbook right at your fingertips. </h1>
-    <h2 className="Header__Small"> Let us help you cater to your consumer's demands by using our advanced recommendation system based on over <strong>50 million</strong> products.</h2>
-    <Button variant="contained" color="secondary" className="Header__Button">DEMO</Button>
-    </div>
-    <div className="Center_Image">
-    <img className="Image--first" src="https://searchengineland.com/figz/wp-content/seloads/2015/10/graph-line-trend-analytics-magnifying-glass-ss-1920.jpg"></img>
-    </div>
-    </div>
-    <section className="section__features">
-      <div className="wrapper">
-      <div className="Header--first">
-    <h1 className="Header__Big--2">How it works</h1>
-    <h2></h2>
-    </div>
-    </div>
-    </section>
+      <Navbar />
+      <div class="wrapper">
+        <div className="Header--first">
+          <h1 className="Header__Big"> Your product playbook right at your fingertips. </h1>
+          <h2 className="Header__Small"> Let us help you cater to your consumer's demands by using our advanced recommendation system based on over <strong>50 million</strong> products.</h2>
+          <Button variant="contained" color="secondary" className="Header__Button">DEMO</Button>
+        </div>
+        <div className="Center_Image">
+          <img className="Image--first" src="https://searchengineland.com/figz/wp-content/seloads/2015/10/graph-line-trend-analytics-magnifying-glass-ss-1920.jpg"></img>
+        </div>
+      </div>
+      <section className="section__features">
+        <div className="wrapper">
+          <div className="Header--first">
+            <h1 className="Header__Big--2">How does StreamLit work?</h1>
+            <Grid container direction="row">
+          <Grid item xs={3}>
+          <h1>1.</h1>
+          <h3>Upload your data</h3>
+          </Grid>
+          <Grid item xs={3}>
+          <h1>2.</h1>
+          <h3>Our model will get to work</h3>
+          </Grid>
+          <Grid item xs={3}>
+          <h1>3.</h1>
+          <h3>Search trends by product</h3>
+          </Grid>
+          </Grid>
+            <h2></h2>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
