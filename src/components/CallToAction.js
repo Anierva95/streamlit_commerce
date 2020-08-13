3aeb3d2485efffbde9cdc736238f7d11367b845d
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-08-13 14:33:15 -0400

    adding a second button to CTA

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -6,8 +6,9 @@ export default function CallToAction(props) {
     return(
         <div className="CallToAction">
             <div className="CallToAction__Box">
-                <h1 className="CallToAction__Header">Ready to get a headstart?</h1>
+                <h1 className="CallToAction__Header">Ready to get a headstart on your business?</h1>
                 <Button className="CallToAction__Button" variant="contained" color="secondary">Demo</Button>
+                <Button className="CallToAction__Button--2" variant="outlined" color="secondary">Pricing</Button>
             </div>
         </div>
     );
